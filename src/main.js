e8df1a2e896358ba1a6c0849126a62642e24453e
lingjiatong <935188400@qq.com>
2023-04-11 17:03:44 +0800

    [feat]路由改为动态路由

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import Vue from "vue";
-import App from "./App.vue";
 import Router from "vue-router";
-import router from "./router";
 import VueAxios from "vue-axios";
 import ElementUI from "element-ui";
-import "@a/css/style.min.css";
-import "@a/iconfont/iconfont.css";
 import "element-ui/lib/theme-chalk/index.css";
-import axios from "@/config/axiosConfig";
 import mavonEditor from "mavon-editor";
 import "mavon-editor/dist/css/index.css";
-import store from "./store"
+import App from "@/App.vue";
+import router from "@/router";
+import "@a/css/style.min.css";
+import "@a/iconfont/iconfont.css";
+import axios from "@/config/axiosConfig";
+import store from "@/store"
 import * as echarts from "echarts";
 import DateUtil from "@/util/dateUtil";
+import {BASE_URL} from "@/constant/commonConstant";
 
 Vue.prototype.$echarts = echarts;
 Vue.config.productionTip = false;
@@ -27,8 +28,37 @@ Vue.filter("dateFormat", function(value, style) {
   return DateUtil.format(value, style);
 });
 
-
-
+axios.get(BASE_URL + "/api-backend/route/list").then(res => {
+  let data = res.data.data;
+  data.forEach(route => {
+    let childrenStr = route.children;
+    let children = [];
+    if (childrenStr) {
+      children = JSON.parse(childrenStr).children.map(child => {
+        let componentPath = child.component.substring(child.component.lastIndexOf("/") + 1);
+        return {
+          name: child.name,
+          path: child.path,
+          redirect: child.redirect ? JSON.parse(child.redirect) : null,
+          props: child.props,
+          meta: child.meta,
+          component: () => import("@v/" + componentPath)
+        }
+      });
+    }
+    let componentPath = route.component.substring(route.component.lastIndexOf("/") + 1);
+    let routeConfig = {
+      name: route.name,
+      path: route.path,
+      redirect: route.redirect ? JSON.parse(route.redirect) : null,
+      props: route.props,
+      meta: JSON.parse(route.meta),
+      children: children,
+      component: () => import("@v/" + componentPath)
+    };
+    router.addRoute(routeConfig);
+  });
+});
 
 new Vue({
   render: (h) => h(App),
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Router from "vue-router";
-// import routes from "./routers";
-import axios from "@/config/axiosConfig";
-import {BASE_URL} from "@/constant/commonConstant";
-import { ROUT_HOME_NAME } from "@/constant/commonConstant";
+import staticRoutes from "@/router/routers";
+import {ROUT_HOME_NAME} from "@/constant/commonConstant";
 import store from "@/store";
 import RouteUtil from "@/util/routeUtil";
 import ElementUI from "element-ui";
-
 // 创建导航，使用历史模式
 const router = new Router({
-  routes: [],
+  // 导入静态路由
+  routes: staticRoutes,
   base: "/",
   mode: "history",
 });
@@ -20,25 +18,6 @@ const originalPush = Router.prototype.push;
 Router.prototype.push = function push(location) {
   return originalPush.call(this, location).catch((err) => err);
 }
-
-// 动态获取路由列表
-axios.get(BASE_URL + "/api-backend/route/list").then(res => {
-  let data = res.data.data;
-  for (let i = 0; i < data.length; i++) {
-    let route = data[i];
-    router.addRoute({
-      name: route.name,
-      path: route.path,
-      // TODO 这里redirect可能为字符串，也可能是一个json对象，需要进行判断然后处理
-      redirect: route.redirect ? JSON.parse(route.redirect) : null,
-      props: route.props,
-      alias: route.alias,
-      meta: route.meta,
-      component: route.component == null ? null :  () => import(route.component.replace(/^.*\('@(.+)'\).*$/, "$1"))
-    });
-  }
-});
-
 // 设置路由守卫
 router.beforeEach((to, from, next) => {
   let toPath = to.path;
@@ -49,7 +28,7 @@ router.beforeEach((to, from, next) => {
 
   // 如果token存在，并且路由路径为/,那么直接跳转到工作台页面
   if (toPath === "/" + ROUT_HOME_NAME) {
-    next({ name: "Workspace" });
+    next({name: "Workspace"});
   }
   // 除了特殊页面之外，如果跳转到正常页面，需要校验token等信息是否存在
   if (!SPECIAL_PAGES.includes(toPath)) {
@@ -65,7 +44,7 @@ router.beforeEach((to, from, next) => {
         duration: 2000,
         center: false
       });
-      next({ name: "Login" });
+      next({name: "Login"});
     }
   }
 
